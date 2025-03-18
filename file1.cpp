1e1e8154e123c7e51bcca384b230c52ef4f0f5b5
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-03-18 17:05:31 +0700

    perulangan untuk pencacah naik

--- file1.cpp
+++ file1.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    int cacah;
+    // perulangan for untuk pencacah naik
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "Nilai Pencacah = " << cacah << " Selamat Datang!" << endl;
+    }
